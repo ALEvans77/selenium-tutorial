cf4c56d70f886c67208fcdabb290312d909ad3e2
ALEvans77 <andy.lee.evans@live.co.uk>
2019-02-20 10:05:49 +0000

    add in first fail test with Junit

---
+++ src/test/java/uk/co/bpdts/selenium/tutorial/AmazonAddToBagTest.java
@@ -0,0 +1,14 @@
+package uk.co.bpdts.selenium.tutorial;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class AmazonAddToBagTest {
+
+    @Test
+    public void testSearchForProductAndAddToBasket() {
+
+        fail("First Test Failure");
+    }
+}
